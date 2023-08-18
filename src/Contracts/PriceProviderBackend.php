897604b8a045f933e1278b33de3cce6008a39377
recursive_tree <recursivetreemail@gmail.com>
2023-08-18 23:00:20 +0200

    move priceProviderBackend to an interface

--- src/Contracts/PriceProviderBackend.php
+++ src/Contracts/PriceProviderBackend.php
@@ -5,45 +5,17 @@ namespace RecursiveTree\Seat\PricesCore\Contracts;
 use Illuminate\Support\Collection;
 use RecursiveTree\Seat\PricesCore\Exceptions\PriceProviderException;
 
-abstract class PriceProviderBackend
+interface PriceProviderBackend
 {
-    protected array $configuration;
-
-    /**
-     * @param array $configuration
-     */
-    public function __construct(array $configuration)
-    {
-        $this->configuration = $configuration;
-    }
-
-    /**
-     * Public interface to get prices from this price provider
-     *
-     * @param Collection<Priceable> $items
-     * @return Collection<Priceable>
-     * @throws PriceProviderException
-     */
-    //TODO if generic type hints ever become available, use them here
-    public final function getPrices(Collection $items): Collection {
-        $this->getPricesInternal($items);
-        return $items;
-    }
-
     /**
      * Fetches the prices for the items in $items
      * Implementations should store the computed price directly on the Priceable object using the setPrice method.
-     * In case an error occurs, a PriceProviderException should be thrown, so that a error message can be shown to the user.
+     * In case an error occurs, a PriceProviderException should be thrown, so that an error message can be shown to the user.
      *
      * @param Collection<Priceable> $items The items to appraise
+     * @param array $configuration The configuration of this price provider backend.
      * @throws PriceProviderException
      */
-    /*
-     * This is a second, internal function that backends should implement.
-     * The reason we have a second function is that we can implement caching at price provider level in the future
-     * without any breaking changes, because we can just inject them into the public getPrices method.
-     */
     //TODO if generic type hints ever become available, use them here
-    protected abstract function getPricesInternal(Collection $items): void;
-
+    public function getPrices(Collection $items, array $configuration): void;
 }
--- src/PriceProviders/SeatPricesPriceProvider.php
+++ src/PriceProviders/SeatPricesPriceProvider.php
@@ -6,25 +6,24 @@ use RecursiveTree\Seat\PricesCore\Contracts\Priceable;
 use RecursiveTree\Seat\PricesCore\Contracts\PriceProviderBackend;
 use Seat\Eveapi\Models\Sde\InvType;
 
-class SeatPricesPriceProvider extends PriceProviderBackend
+class SeatPricesPriceProvider implements PriceProviderBackend
 {
 
     /**
-     * This is a second, internal function that backends should implement.
-     * The reason we have a second function is that we can implement caching at price provider level in the future
-     * without any breaking changes, because we can just inject them into the public getPrices method.
-     * Implementations store the computed price directly on the Priceable object.
-     * This allows some optimisations compared to returning new priceable objects when implementing priceable on a model.
+     * Fetches the prices for the items in $items
+     * Implementations should store the computed price directly on the Priceable object using the setPrice method.
+     * In case an error occurs, a PriceProviderException should be thrown, so that an error message can be shown to the user.
      *
      * @param Collection<Priceable> $items The items to appraise
+     * @param array $configuration The configuration of this price provider backend.
      */
-    protected function getPricesInternal(Collection $items): void
+    public function getPrices(Collection $items, array $configuration): void
     {
-        $price_type = $this->configuration['price_type'];
+        $price_type = $configuration['price_type'];
 
         foreach ($items as $item) {
             $invType = InvType::with('price')->where("typeID",$item->getTypeID())->first();
-            $price = $invType->price->$price_type ?? $invType->basePrice ?? 9;
+            $price = $invType->price->$price_type ?? $invType->basePrice ?? 0;
             $item->setPrice($price * $item->getAmount());
         }
     }
