5d09559f870b81c3a012d0bb071128cfa1d5160c
recursive_tree <recursivetreemail@gmail.com>
2023-08-18 12:16:43 +0200

    change sidebar icon

--- src/Config/package.sidebar.settings.php
+++ src/Config/package.sidebar.settings.php
@@ -1,28 +1,10 @@
 <?php
 
-//return [
-//    'pricescore' => [
-//        'name' => 'pricescore',
-//        'label' => 'pricescore::settings.price_provider',
-//        //'permission' => 'pricescore.settings',
-//        'icon' => 'fas fa-cogs',
-//        'route_segment' => 'prices-core',
-//        'entries' => [
-//            [
-//                'name' => 'pricescore::settings',
-//                'label' => 'pricescore::settings.price_provider_settings',
-//                'icon' => 'fas fa-key',
-//                'route' => 'pricescore::settings'
-//            ],
-//        ],
-//    ]
-//];
-
 return [
     [
         'name' => 'pricescore::settings',
         'label' => 'pricescore::settings.price_provider_settings',
-        'icon' => 'fas fa-key',
+        'icon' => 'fas fa-coins',
         'route' => 'pricescore::settings'
     ],
 ];
